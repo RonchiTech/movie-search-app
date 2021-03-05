c2deb0b01ecc1735e1af7e9ec46e274b73849563
ronchitech <ronchibusiness@gmail.com>
2021-03-05 18:56:03 +0800

    changed anchor to Link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Main from './components/Main/Main';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
-  return <Main />;
+  return (
+    <Router>
+      <Main />
+    </Router>
+  );
 }
 
 export default App;
--- src/UI/Card/Card.jsx
+++ src/UI/Card/Card.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const card = ({src,alt,title_name,overview}) => {
     return (
       <div className="Card">
-        <img
-          src={src}
-          alt={alt}
-        />
+        <img src={src} alt={alt} />
         <h2 className="poster-name">{title_name}</h2>
         <p>
           <span className="truncate">{overview}</span>
-          <a href="#">Read more</a>
+          <Link>Read more</Link>
         </p>
       </div>
     );
