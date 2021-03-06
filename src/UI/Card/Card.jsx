cee8c920078d85fb7a4ef0f7f0875e92d5674929
ronchitech <ronchibusiness@gmail.com>
2021-03-06 10:03:23 +0800

    changed the class truncate width

--- src/UI/Card/Card.jsx
+++ src/UI/Card/Card.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-const card = ({src,alt,title_name,overview}) => {
-    return (
-      <div className="Card">
-        <img src={src} alt={alt} />
-        <h2 className="poster-name">{title_name}</h2>
-        <p>
-          <span className="truncate">{overview}</span>
-          <Link>Read more</Link>
-        </p>
-      </div>
-    );
-}
+import React from 'react';
+import { Link } from 'react-router-dom';
+const card = ({ src, alt, title_name, overview }) => {
+  return (
+    <div className="Card">
+      <img src={src} alt={alt} />
+      <h2 className="poster-name">{title_name}</h2>
+      <p>
+        <span className="truncate">{overview}</span>
+        <Link>Read more</Link>
+      </p>
+    </div>
+  );
+};
 export default card;
