10f40e720427d03593d1af70c67eb2ca9a34026d
ronchitech <ronchibusiness@gmail.com>
2021-03-07 09:20:18 +0800

    added read more/less handler

--- src/UI/Card/Card.jsx
+++ src/UI/Card/Card.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-const card = ({ src, alt, title_name, overview }) => {
+import React, { useState } from 'react';
+
+const Card = ({ src, alt, title_name, overview, readingMore }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const readMoreHandler = () => {
+    setReadMore((currValue) => !currValue);
+  };
+
+  let truncateStyle = {
+    display: 'inline-block',
+    width: '95%',
+    whiteSpace: 'pre',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  };
+  const paragraphStyle = {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    margin: '-15px 0 0 10px',
+  };
+  let cardStyle = {};
+  if (readMore) {
+    truncateStyle = {
+      ...truncateStyle,
+      whiteSpace: 'normal',
+      height: 'auto',
+      textAlign: 'justify',
+      // textJustify: 'auto'
+    };
+    cardStyle = {
+      overflowY: 'scroll',
+    };
+  }
   return (
-    <div className="Card">
+    <div className="Card" style={cardStyle}>
       <img src={src} alt={alt} />
       <h2 className="poster-name">{title_name}</h2>
       <p>
-        <span className="truncate">{overview}</span>
-        <Link to='/'>Read more</Link>
+        <span className="truncate" style={truncateStyle}>
+          {overview}
+        </span>
+        <p
+          className="read-more"
+          onClick={readMoreHandler}
+          readingMore={readMore}
+          style={paragraphStyle}
+        >
+          {readMore ? 'Read less' : 'Read more'}
+        </p>
       </p>
     </div>
   );
 };
-export default card;
+export default Card;
